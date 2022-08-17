f244ea45df7f03bba3b8ef59e7af72f4166fa054
olgushe4ka <olgushe4ka@gmail.com>
2022-08-17 22:22:33 +0300

    Добавила JS в секцию партнеров

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import './index.css';
-import imgLogoScroll from '../src/images/logo/logo-scroll.png';
+import "./index.css";
+import imgLogoScroll from "../src/images/logo/logo-scroll.png";
 
-import { enableChangeHeaderByScroll } from './components/header';
+import { enableChangeHeaderByScroll } from "./components/header";
+import { addPartners, partnersList } from "./components/partners";
 
+enableChangeHeaderByScroll({ newImage: imgLogoScroll });
 
-
-enableChangeHeaderByScroll({newImage: imgLogoScroll});
+//Секция с партнерами
+partnersList.forEach((item) => {
+    const partnersContainer = document.querySelector(".partners__logo-box");
+    addPartners(item, partnersContainer);
+  });
