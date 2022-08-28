056718c1a5160785e68a8bb4634f4a48dbd96935
Alexey Yanchev <yanchevalexey@yandex.ru>
2022-08-28 22:03:15 +0300

    добавил тег nav

--- src/components/header.js
+++ src/components/header.js
@@ -86,8 +86,8 @@ export class Header {
                 this.headerLogoContainer.classList.add('header__logo-container_position_center');
                 this.headerLogo.classList.add('logo_open-menu-image');
                 this.header.classList.add('header_open-menu');
-                this.headerMenuLinks.classList.add('header__menu-links_open-menu');
-                this.headerMenuLinksItem.classList.add('header__menu-links-item_open-menu');
+                // this.headerMenuLinks.classList.add('header__menu-links_open-menu');
+                // this.headerMenuLinksItem.classList.add('header__menu-links-item_open-menu');
 
                 // Если был скролл
                 if (this.scrollFlag) {
@@ -100,8 +100,8 @@ export class Header {
                 this.headerLogoContainer.classList.remove('header__logo-container_position_center');
                 this.headerLogo.classList.remove('logo_open-menu-image');
                 this.header.classList.remove('header_open-menu');
-                this.headerMenuLinks.classList.remove('header__menu-links_open-menu');
-                this.headerMenuLinksItem.classList.remove('header__menu-links-item_open-menu');
+                // this.headerMenuLinks.classList.remove('header__menu-links_open-menu');
+                // this.headerMenuLinksItem.classList.remove('header__menu-links-item_open-menu');
 
                 // Если был скролл
                 if (this.scrollFlag) {
@@ -112,8 +112,8 @@ export class Header {
             } else if (window.matchMedia('(min-width: 1110px)').matches 
             && !this.menuToggle.checked) {
                 this.header.classList.add('header_open-menu');
-                this.headerMenuLinks.classList.add('header__menu-links_open-menu');
-                this.headerMenuLinksItem.classList.add('header__menu-links-item_open-menu');
+                // this.headerMenuLinks.classList.add('header__menu-links_open-menu');
+                // this.headerMenuLinksItem.classList.add('header__menu-links-item_open-menu');
 
                 // Если был скролл
                 if (this.scrollFlag) {
@@ -124,8 +124,8 @@ export class Header {
             } else if (window.matchMedia('(min-width: 1110px)').matches 
             && this.menuToggle.checked) {
                 this.header.classList.remove('header_open-menu');
-                this.headerMenuLinks.classList.remove('header__menu-links_open-menu');
-                this.headerMenuLinksItem.classList.remove('header__menu-links-item_open-menu');
+                // this.headerMenuLinks.classList.remove('header__menu-links_open-menu');
+                // this.headerMenuLinksItem.classList.remove('header__menu-links-item_open-menu');
 
                 // Если был скролл
                 if (this.scrollFlag) {
