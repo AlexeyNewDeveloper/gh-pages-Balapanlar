81393b7d82575d7ceafdb2c0609888bac960481a
Arseniy Sazonov <rimus00@yandex.ru>
2022-08-28 17:48:25 +0300

    Доделать верстку

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -1,20 +1,28 @@
-import {gsap} from 'gsap';
+import { gsap } from "gsap";
 const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
 gsap.registerPlugin(ScrollTrigger);
 
-const sections = Array.from(document.querySelectorAll('.principles'));
-const width = (sections[0].offsetWidth - 110) * (sections.length - 1)
+const sections = Array.from(document.querySelectorAll(".principles"));
+function start() {
+  if (window.outerWidth > 750) {
+      const width = (sections[0].offsetWidth - 5) * (sections.length - 1);
+      const tl = gsap.timeline();
+      tl.fromTo(sections, { x: 0 }, { x: -width });
 
-export const  scroll = () =>  {
-    const tl = gsap.timeline();
-    tl.fromTo(sections, {x: 0}, {x: -width})
-    
-    ScrollTrigger.create({
-        animation: tl,
-        trigger: '.scroll-container',
-        start: 'top top',
-        end: 'bottom',
-        scrub: 1,
-        pin: true,
-    })
+      ScrollTrigger.create({
+          animation: tl,
+          trigger: ".scroll-container",
+          start: "top top",
+          end: "bottom",
+          scrub: 1,
+          pin: true,
+      });
+  } else {
+    ScrollTrigger.killAll()
+  }
 }
+
+export const scroll = () => {
+  start()
+  window.addEventListener("resize", start);
+};
